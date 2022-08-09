8e61b1992f1c5c7af1cc22ef6d4365fe483855a2
leila valencia <levamo26@outlook.com>
2022-08-09 13:18:09 -0500

    programacion refuerzo

---
+++ src/RefuerzoProgramacion.java
@@ -0,0 +1,7 @@
+public class RefuerzoProgramacion {
+    public static void main (String [] args){
+        System.out.println("refuerzo en programacion");
+        System.out.println("curso java");
+
+    }
+}
